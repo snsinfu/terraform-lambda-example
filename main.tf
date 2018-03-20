c0cac914396217feee8289e8a4c7663d88ffa86b
snsinfu <snsinfu@gmail.com>
2018-03-21 02:19:30 +0900

    Style main.tf

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_api_gateway_method" "hello" {
   rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
   resource_id = "${aws_api_gateway_resource.hello.id}"
   http_method = "GET"
-	authorization = "NONE"
+  authorization = "NONE"
 }
 
 #              POST
