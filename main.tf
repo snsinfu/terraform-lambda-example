7aad7f44a64281873055b9309ac83d718cb7e19a
snsinfu <snsinfu@gmail.com>
2018-10-16 20:32:14 +0900

    Style terraform configuration

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ data "aws_region" "current" {
 #
 # The handler is the name of the executable for go1.x runtime.
 resource "aws_lambda_function" "hello" {
-  function_name = "hello"
-  filename = "hello.zip"
-  handler = "hello"
+  function_name    = "hello"
+  filename         = "hello.zip"
+  handler          = "hello"
   source_code_hash = "${base64sha256(file("hello.zip"))}"
-  role = "${aws_iam_role.hello.arn}"
-  runtime = "go1.x"
-  memory_size = 128
-  timeout = 1
+  role             = "${aws_iam_role.hello.arn}"
+  runtime          = "go1.x"
+  memory_size      = 128
+  timeout          = 1
 }
 
 # A Lambda function may access to other AWS resources such as S3 bucket. So an
@@ -24,7 +24,7 @@ resource "aws_lambda_function" "hello" {
 #
 # [1]: https://docs.aws.amazon.com/IAM/latest/UserGuide/reference_policies_elements_version.html
 resource "aws_iam_role" "hello" {
-  name = "hello"
+  name               = "hello"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -41,18 +41,18 @@ POLICY
 
 # Allow API gateway to invoke the hello Lambda function.
 resource "aws_lambda_permission" "hello" {
-  statement_id = "AllowAPIGatewayInvoke"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.hello.arn}"
-  principal = "apigateway.amazonaws.com"
+  principal     = "apigateway.amazonaws.com"
 }
 
 # A Lambda function is not a usual public REST API. We need to use AWS API
 # Gateway to map a Lambda function to an HTTP endpoint.
 resource "aws_api_gateway_resource" "hello" {
   rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
-  parent_id = "${aws_api_gateway_rest_api.hello.root_resource_id}"
-  path_part = "hello"
+  parent_id   = "${aws_api_gateway_rest_api.hello.root_resource_id}"
+  path_part   = "hello"
 }
 
 resource "aws_api_gateway_rest_api" "hello" {
@@ -62,9 +62,9 @@ resource "aws_api_gateway_rest_api" "hello" {
 #           GET
 # Internet -----> API Gateway
 resource "aws_api_gateway_method" "hello" {
-  rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
-  http_method = "GET"
+  rest_api_id   = "${aws_api_gateway_rest_api.hello.id}"
+  resource_id   = "${aws_api_gateway_resource.hello.id}"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -74,12 +74,12 @@ resource "aws_api_gateway_method" "hello" {
 #
 # The date 2015-03-31 in the URI is just the version of AWS Lambda.
 resource "aws_api_gateway_integration" "hello" {
-  rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
-  resource_id = "${aws_api_gateway_resource.hello.id}"
-  http_method = "${aws_api_gateway_method.hello.http_method}"
+  rest_api_id             = "${aws_api_gateway_rest_api.hello.id}"
+  resource_id             = "${aws_api_gateway_resource.hello.id}"
+  http_method             = "${aws_api_gateway_method.hello.http_method}"
   integration_http_method = "POST"
-  type = "AWS_PROXY"
-  uri = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.hello.arn}/invocations"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.hello.arn}/invocations"
 }
 
 # This resource defines the URL of the API Gateway.
@@ -88,7 +88,7 @@ resource "aws_api_gateway_deployment" "hello_v1" {
     "aws_api_gateway_integration.hello"
   ]
   rest_api_id = "${aws_api_gateway_rest_api.hello.id}"
-  stage_name = "v1"
+  stage_name  = "v1"
 }
 
 # Set the generated URL as an output. Run `terraform output url` to get this.
